8cb3a2f63d0845cac85384d7dd7ca93911a11167
swanduron <swanduron@hotmail.com>
2018-02-20 20:21:58 +0800

    Add ds3231 lib in project.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A clock with micrpython and DL1414 LED display
 # ds3231
 This is a base lib for DS3231 chip, it uses iic bus and can be access with a very simple way.
 
+# BME280
+This lib for driving BME280 sensor. This sensor use IIC bus and has very quickly probe periodic.
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import machine
+import time
+
+import bme280
 from dl1414 import Dl1414
+from ds3231 import DS3231
+
+if __name__ == '__main__':
+    print('Simple for bme280')
+    i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
+    bme = bme280.BME280(i2c=i2c)
+    print(bme.values)
+
+    print('Simple for DS3231')
+    ds = DS3231()
+    ds.DATE([17, 9, 1])
+    ds.TIME([10, 10, 10])
+
+    while True:
+        print('Date:', ds.DATE())
+        print('Time:', ds.TIME())
+        print('TEMP:', ds.TEMP())
+        time.sleep(5)
